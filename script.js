2a554796378c67e1be77f2254aad921b69d60943
ESTUDIANTES <ESTUDIANTES@puce.edu.ec>
2025-08-07 15:55:53 -0500

    CAMBIO

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("miBoton").addEventListener("click", function () {
+    const mensaje = document.getElementById("mensaje");
+    mensaje.textContent = "¡Has hecho clic en el botón!";
+  });
+  
